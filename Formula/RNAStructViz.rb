4880116c110186c7d608814cabf852b3164f6f06
Maxie D. Schmidt <maxieds@gmail.com>
2018-11-01 07:24:10 -0400

    Next iteration of the RNAStructViz.rb brew formula ... here goes

--- Formula/RNAStructViz.rb
+++ Formula/RNAStructViz.rb
@@ -5,7 +5,7 @@ class RNAStructViz < Formula
   version "v1.1-with-cairo (prerelease)"
   sha256 "09ea8ae57aa5a5c0e017607d69e4beba0227181e431a9cbb54c1dfa5d082e3b3"
 
-  depend_on "gtDMMB/core/fltkwithcairo"
+  depends_on "gtDMMB/core/fltkwithcairo"
   depends_on "pkg-config"
   depends_on "git"
   depends_on "coreutils"
