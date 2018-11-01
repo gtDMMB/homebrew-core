6d5d4e36f5e0958fe542724a3f2734179cc34f98
Maxie D. Schmidt <maxieds@gmail.com>
2018-11-01 07:46:08 -0400

    Debugging the brew formula

--- Formula/RNAStructViz.rb
+++ Formula/RNAStructViz.rb
@@ -1,9 +1,9 @@
 class Rnastructviz < Formula
   desc "CT file viewer and RNA structure comparison tool"
   homepage "https://github.com/gtDMMB/RNAStructViz"
-  url "https://github.com/gtDMMB/RNAStructViz/releases/download/v1.1-with-cairo/RNAStructViz-source.tar.gz"
+  url "https://github.com/gtDMMB/RNAStructViz/releases/download/v1.1-with-cairo/RNAStructViz-source-v2.tar.gz"
   version "1.1-with-cairo"
-  sha256 "2064315389d62021b62c1d354df4dbf58f15811ae0a904081fd4f4562841ba98"
+  sha256 "802a809406cf41c28f86b8e602e1a950fd3af12f866ab907b9cc7a1619f7ac9b"
 
   depends_on "gtDMMB/core/fltkwithcairo"
   depends_on "pkg-config"
