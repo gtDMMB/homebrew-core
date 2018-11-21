163859c2f4f71c8ec32c0ce7de78a43668925b9e
Maxie D. Schmidt <maxieds@gmail.com>
2018-11-21 17:42:59 -0500

    Updated the brew formula to run git on the release version

--- Formula/RNAStructViz.rb
+++ Formula/RNAStructViz.rb
@@ -1,16 +1,31 @@
 class Rnastructviz < Formula
+  ## Usage: getLatestReleaseURL("https://api.github.com/repos/gtDMMB/RNAStructViz/releases/latest")
+  #def self.getLatestReleaseURL(gitHubJSONURL)
+  #  getCmd = "curl -s -m 20 -X GET " + gitHubJSONURL + "| " + 
+  #           "sed -n 's/\"tarball_url\": \"\(.*\)\",.*$/\1/p' | " + 
+  #           "sed -e 's/ //g'"
+  #  latestReleaseURL = `#{getCmd}`.tr("\n", "")
+  #  return latestReleaseURL
+  #end
+
   desc "CT file viewer and RNA structure comparison tool"
   homepage "https://github.com/gtDMMB/RNAStructViz"
-  url "https://github.com/gtDMMB/RNAStructViz/releases/download/v1.1-with-cairo/RNAStructViz-source-v4.tar.gz"
-  version "1.1-v4-with-cairo"
-  sha256 "f98141151dc15abc954e514d3e76a477408e648a9dc38ec75419ea270cacdd40"
+  url "https://api.github.com/repos/gtDMMB/RNAStructViz/tarball/v1.1-with-cairo-beta"
+  version "1.1-with-cairo-beta"
+  revision 2
 
   depends_on "gtDMMB/core/fltkwithcairo"
   depends_on "pkg-config"
   depends_on "git"
+  depends_on "curl"
   depends_on "coreutils"
 
   def install
+    system "git", "clone", "https://github.com/gtDMMB/RNAStructViz.git"
+    Dir.chdir("RNAStructViz")
+    system "git", "fetch", "origin", "f49a506e7b6b420cbe67a65e1538f5a0162ff850"
+    system "git", "reset", "--hard", "FETCH_HEAD"
+    system "git", "checkout", "with-cairo"
     system "make", "clean"
     system "make"
     system "make", "install", "INSTALL_PREFIX=#{prefix}"
