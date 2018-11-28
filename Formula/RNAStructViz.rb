8dea8fba5f288c7aca5e83a4cb3fa132ffb9dfae
Maxie D. Schmidt <maxieds@gmail.com>
2018-11-28 15:43:40 -0500

    Upgrade paths and commit info to the latest release

--- Formula/RNAStructViz.rb
+++ Formula/RNAStructViz.rb
@@ -1,18 +1,9 @@
 class Rnastructviz < Formula
-  ## Usage: getLatestReleaseURL("https://api.github.com/repos/gtDMMB/RNAStructViz/releases/latest")
-  #def self.getLatestReleaseURL(gitHubJSONURL)
-  #  getCmd = "curl -s -m 20 -X GET " + gitHubJSONURL + "| " + 
-  #           "sed -n 's/\"tarball_url\": \"\(.*\)\",.*$/\1/p' | " + 
-  #           "sed -e 's/ //g'"
-  #  latestReleaseURL = `#{getCmd}`.tr("\n", "")
-  #  return latestReleaseURL
-  #end
-
   desc "CT file viewer and RNA structure comparison tool"
   homepage "https://github.com/gtDMMB/RNAStructViz"
-  url "https://api.github.com/repos/gtDMMB/RNAStructViz/tarball/v1.1-with-cairo-beta"
-  version "1.1-with-cairo-beta"
-  revision 2
+  url "https://api.github.com/repos/gtDMMB/RNAStructViz/tarball/v1.2-snapshot-beta"
+  version "v1.2-snapshot-beta"
+  revision 1
 
   depends_on "gtDMMB/core/fltkwithcairo"
   depends_on "pkg-config"
@@ -21,9 +12,10 @@ class Rnastructviz < Formula
   depends_on "coreutils"
 
   def install
+    latestRevCommitSHA = "f49a506e7b6b420cbe67a65e1538f5a0162ff850"
     system "git", "clone", "https://github.com/gtDMMB/RNAStructViz.git"
     Dir.chdir("RNAStructViz")
-    system "git", "fetch", "origin", "f49a506e7b6b420cbe67a65e1538f5a0162ff850"
+    system "git", "fetch", "origin", latestRevCommitSHA
     system "git", "reset", "--hard", "FETCH_HEAD"
     system "git", "checkout", "with-cairo"
     system "make", "clean"
