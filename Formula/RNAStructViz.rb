69d970b85190a2c8b86aa78942e3d9b065e36e85
Maxie D. Schmidt <maxieds@gmail.com>
2018-11-29 01:47:15 -0500

    Install both the latest release and the active snapshot of git by default to separate binaries (mostly for testing)

--- Formula/RNAStructViz.rb
+++ Formula/RNAStructViz.rb
@@ -2,8 +2,9 @@ class Rnastructviz < Formula
   desc "CT file viewer and RNA structure comparison tool"
   homepage "https://github.com/gtDMMB/RNAStructViz"
   url "https://api.github.com/repos/gtDMMB/RNAStructViz/tarball/v1.2-snapshot-beta"
+  sha256 "7724b875c2b9ec70f9677be18bc1fe496ec25cd72707171595b12b16a8ceafb2"
   version "v1.2-snapshot-beta"
-  revision 1
+  revision 2
 
   depends_on "gtDMMB/core/fltkwithcairo"
   depends_on "pkg-config"
@@ -20,6 +21,16 @@ class Rnastructviz < Formula
     system "git", "checkout", "with-cairo"
     system "make", "clean"
     system "make"
+    system "make", "install", "INSTALL_PREFIX=#{prefix}"
+    system "make", "clean"
+    system "git", "fetch", "origin", "with-cairo"
+    system "git", "reset", "--hard", "FETCH_HEAD"
+    system "make"
+    Dir.chdir("src/")
+    system "mv", "RNAStructViz", "RNAStructViz-unstable-latest"
+    system "sed", "-i", "mkout",
+           "s/BINARY_OUTPUT=RNAStructViz/BINARY_OUTPUT=RNAStructViz-unstable-latest/", "Makefile"
+    Dir.chdir("../")    
     system "make", "install", "INSTALL_PREFIX=#{prefix}"
   end
 
