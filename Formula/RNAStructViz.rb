7a57f9b369177365bd97df25946e63a5fdeb1d87
Maxie D. Schmidt <maxieds@gmail.com>
2019-02-22 17:30:09 -0500

    v1.3.5-production

--- Formula/RNAStructViz.rb
+++ Formula/RNAStructViz.rb
@@ -1,9 +1,9 @@
 class Rnastructviz < Formula
   desc "CT file viewer and graphical RNA structure comparison tool"
   homepage "https://github.com/gtDMMB/RNAStructViz/wiki"
-  url "https://github.com/gtDMMB/RNAStructViz/archive/v1.3.4-production.tar.gz"
-  sha256 "77e9b408c5ecdeda94be2ac94c9918a74241ed0b7e3b4dbd4d0438e7f6444356"
-  version "v1.3.4-production"
+  url "https://github.com/gtDMMB/RNAStructViz/archive/v1.3.5-production.tar.gz"
+  sha256 "5ec26cc965f21a4e8f9b03604566dc2d8b6aef14ef0db3dffdee43acb4b7bf01"
+  version "v1.3.5-production"
   revision 1
 
   depends_on "gtDMMB/core/fltkwithcairo"
