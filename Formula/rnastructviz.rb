16ad21cb14e3dd8fbbd6f383ccf8e95901553fd1
Maxie D. Schmidt <maxieds@gmail.com / mschmidt34@gatech.edu>
2019-03-03 00:25:32 -0500

    Remaming rnastructviz to all lowercase caps

---
+++ Formula/rnastructviz.rb
@@ -0,0 +1,26 @@
+class Rnastructviz < Formula
+  desc "CT file viewer and graphical RNA structure comparison tool"
+  homepage "https://github.com/gtDMMB/RNAStructViz/wiki"
+  url "https://github.com/gtDMMB/RNAStructViz/archive/v1.3.9-production.tar.gz"
+  sha256 "db0c58626d7ac143201da9cb341e6c171ac3247a016d76d7c2f509dd165dd008"
+  version "v1.3.9-production"
+  revision 1
+
+  depends_on "gtDMMB/core/fltkwithcairo"
+  depends_on "pkg-config"
+  depends_on "git"
+  depends_on "curl"
+  depends_on "coreutils"
+
+  def install
+    system "git", "clone", "https://github.com/gtDMMB/RNAStructViz.git"
+    Dir.chdir("RNAStructViz")
+    system "make", "clean"
+    system "make"
+    system "make", "install", "INSTALL_PREFIX=#{prefix}"
+  end
+
+  test do
+    system "false"
+  end
+end
