00904c25fb4f5d479766a1d0a8cfd7c3fbeba549
Maxie D. Schmidt <maxieds@gmail.com>
2019-03-05 18:22:03 -0500

    rnastructviz -- updated to v1.4.2-testing

--- Formula/rnastructviz.rb
+++ Formula/rnastructviz.rb
@@ -2,8 +2,8 @@ class Rnastructviz < Formula
   desc "CT file viewer and graphical RNA structure comparison tool"
   homepage "https://github.com/gtDMMB/RNAStructViz/wiki"
   url "https://github.com/gtDMMB/RNAStructViz/archive/v1.4.1-testing.tar.gz"
-  sha256 "a7467ac4d47f479560f6e6ba10213f1b1e56ec1c976729ff7be73009a5381aec"
-  version "v1.4.1-testing"
+  sha256 "5b1b60a7541271f8409d7a88fad9cd9d33e6dca5b0aa263a86716edbb0cbf8e9"
+  version "v1.4.2-testing"
   revision 1
 
   depends_on "gtDMMB/core/fltkwithcairo"
@@ -11,12 +11,17 @@ class Rnastructviz < Formula
   depends_on "git"
   depends_on "curl"
   depends_on "coreutils"
+  
+  option :universal
+  option "with-clang", "Use the *clang* compiler instead of the default gcc"
 
   def install
+    makeExportPrefix = ""
+    makeExportPrefix << "STRUCTVIZ_COMPILER=clang++" if build.with? "clang"
     system "git", "clone", "https://github.com/gtDMMB/RNAStructViz.git"
     Dir.chdir("RNAStructViz")
     system "make", "clean"
-    system "make"
+    system makeExportPrefix, "make"
     system "make", "install", "INSTALL_PREFIX=#{prefix}"
   end
 
