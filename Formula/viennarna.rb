921e15db1077e73b37055b2d3d587af902d6c950
Maxie D. Schmidt <maxieds@gmail.com>
2019-03-20 14:27:00 -0400

    Update viennarna.rb

--- Formula/viennarna.rb
+++ Formula/viennarna.rb
@@ -4,13 +4,19 @@ class Viennarna < Formula
   url "https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_4_x/ViennaRNA-2.4.11.tar.gz"
   sha256 "0a8acfc3445da2dbdb42bd306ecdaf44e26ab40ca58987b6917c3899ef9bf310"
   version "2.4.11"
-  revision 16
+  revision 17
 
   depends_on "pkg-config"
   depends_on "mpfr"
   depends_on "wget"
   depends_on "gsl"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-core"
+    cellar :any
+    sha256 "4a09dda7db4153746874f81519fbd00769ef82c830a097466389d2ddb6834622" => :mojave
+  end
+  
   def install
     system "wget", "https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_4_x/ViennaRNA-2.4.11.tar.gz"
     system "tar", "xvzf", "ViennaRNA-2.4.11.tar.gz"
