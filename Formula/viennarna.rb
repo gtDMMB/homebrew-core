776d4a58d1626ee189ccb7b41c551cea795ac528
Maxie D. Schmidt <maxieds@gmail.com>
2019-03-20 12:53:19 -0400

    Update viennarna.rb

--- Formula/viennarna.rb
+++ Formula/viennarna.rb
@@ -4,18 +4,24 @@ class Viennarna < Formula
   url "https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_4_x/ViennaRNA-2.4.11.tar.gz"
   sha256 "0a8acfc3445da2dbdb42bd306ecdaf44e26ab40ca58987b6917c3899ef9bf310"
   version "2.4.11"
-  revision 4
+  revision 5
 
   #fails_with :clang # needs OpenMP
 
   depends_on "gcc" if OS.mac? # for OpenMP
   #depends_on "python@2"
   depends_on "pkg-config"
+  depends_on "mpfr"
 
   def install
-    system "./configure",
+    system "CC=gcc", "CXX=g++", "./configure", "-v",
       "--disable-debug",
       "--disable-dependency-tracking",
+      "--disable-openmp",
+      "--enable-static-executables",
+      " --without-python", 
+      " --without-python3", 
+      " --without-perl",
       "--prefix=#{prefix}"
     system "make"
     #ENV.deparallelize
