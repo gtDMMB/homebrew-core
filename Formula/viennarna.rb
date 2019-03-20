c52c444e8c13bb49225d68f7acee6dfc8fca1d70
Maxie D. Schmidt <maxieds@gmail.com>
2019-03-20 12:39:29 -0400

    Update viennarna.rb

--- Formula/viennarna.rb
+++ Formula/viennarna.rb
@@ -3,7 +3,8 @@ class Viennarna < Formula
   homepage "https://www.tbi.univie.ac.at/RNA/"
   url "https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_4_x/ViennaRNA-2.4.11.tar.gz"
   sha256 "9913fc04a6f2ab380bd492301f9c5a07cfe1b6c9ca437261de03691c3d3d6837"
-  revision 1
+  version "2.4.11"
+  revision 2
 
   fails_with :clang # needs OpenMP
 
