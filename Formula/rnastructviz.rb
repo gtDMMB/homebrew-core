c11299ced6c9fe91d189ebd5e48671a2ec8ef705
Maxie D. Schmidt <maxieds@gmail.com>
2019-03-30 02:25:37 -0400

    Update rnastructviz.rb

--- Formula/rnastructviz.rb
+++ Formula/rnastructviz.rb
@@ -4,7 +4,7 @@ class Rnastructviz < Formula
   url "https://github.com/gtDMMB/RNAStructViz/archive/v1.5.1-testing.tar.gz"
   sha256 "b6bf6d31f039630335322b0884af27b352ddb547b6a21c551649fa05422404e1"
   version "v1.5.1-testing"
-  revision 1
+  revision 2
 
   depends_on "gtDMMB/core/fltkwithcairo"
   depends_on "gtDMMB/core/viennarna"
@@ -12,13 +12,12 @@ class Rnastructviz < Formula
   depends_on "curl"
   depends_on "coreutils"
   
-  #bottle do
-  #  root_url "https://homebrew.bintray.com/bottles-core"
-  #  cellar :any
-  #  sha256 "ea3c8cf3270c5dcc38eb9480cb5475c6f87b9d1ebdd7c9fa10aded6279d166a7" => :mojave
-  #end
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-core"
+    cellar :any
+    sha256 "1a5341c3d05288676cda1b9bcd5199d12cb033bb3142e40fed7712f01f28eadd" => :mojave
+  end
   
-  #option :universal
   option "with-clang", "Use the *clang* compiler instead of the default gcc"
 
   def install
