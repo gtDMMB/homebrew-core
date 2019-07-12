527fcacc1351da68632eb3a52637c13544a11a25
Maxie D. Schmidt <maxieds@gmail.com>
2019-07-11 23:27:01 -0400

    Update rnastructviz.rb

--- Formula/rnastructviz.rb
+++ Formula/rnastructviz.rb
@@ -4,25 +4,15 @@ class Rnastructviz < Formula
   url "https://github.com/gtDMMB/RNAStructViz/archive/v1.6.7-testing.tar.gz"
   sha256 "64d131ddd476a66bf06c438e7d2dc860a6b12f6db958c41ba93988e03ac7c7a0"
   version "v1.6.7-testing"
-  revision 4
+  revision 5
 
   depends_on "gtDMMB/core/fltkwithcairo"
   depends_on "gtDMMB/core/viennarna"
   depends_on "pkg-config"
   depends_on "curl"
   depends_on "coreutils"
-  
-  #option "with-clang", "Use the *clang* compiler instead of the default gcc"
 
   def install
-    #makeExportPrefix = ""
-    #makeExportPrefix << "STRUCTVIZ_COMPILER=clang++" if build.with? "clang"
-    #system "make", "clean"
-    #if makeExportPrefix != ""
-    #  system makeExportPrefix, "make"
-    #elsif
-    #  system "make", "install", "INSTALL_PREFIX=#{prefix}"
-    #end
     system "make"
     bin.install "src/RNAStructViz"
     prefix.install Dir["sample_structures"]
