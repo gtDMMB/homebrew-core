b5c746ca4ddad97cd8e3b71bddd197d528c5dd29
Maxie D. Schmidt <maxieds@gmail.com>
2019-09-06 04:20:15 -0400

    RNAStructViz -- v1.6.9-testing

--- Formula/rnastructviz.rb
+++ Formula/rnastructviz.rb
@@ -1,10 +1,10 @@
 class Rnastructviz < Formula
   desc "Graphical RNA secondary structure comparison tool"
   homepage "https://github.com/gtDMMB/RNAStructViz/wiki"
-  url "https://github.com/gtDMMB/RNAStructViz/archive/v1.6.8-testing.tar.gz"
-  sha256 "3c672fb791b3aa73f60a3a426b2fb35c16b6b5e1caabbccba83bc4ac6c837494"
-  version "v1.6.8-testing"
-  revision 1
+  url "https://github.com/gtDMMB/RNAStructViz/archive/v1.6.9-testing.tar.gz"
+  sha256 "b14d7cf608909a6273c4a99e5cbe8ee1f0c335138675db5668f3ab9c4d4ff873"
+  version "v1.6.9-testing"
+  revision 2
 
   depends_on "gtDMMB/core/fltkwithcairo"
   depends_on "gtDMMB/core/viennarna"
