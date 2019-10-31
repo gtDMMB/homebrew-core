5bad08e99eac560170dffb0ce36a761cab9cd3e4
Maxie D. Schmidt <maxieds@gmail.com>
2019-10-31 18:46:14 -0400

    ViennaRNA -> v2.4.14
     (gcc-8 compatible build formula)

--- Formula/viennarna.rb
+++ Formula/viennarna.rb
@@ -1,27 +1,20 @@
 class Viennarna < Formula
   desc "ViennaRNA and RNAlib: Prediction and comparison of RNA secondary structures"
   homepage "https://www.tbi.univie.ac.at/RNA/"
-  url "https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_4_x/ViennaRNA-2.4.11.tar.gz"
-  #sha256 "bc88998313593dff5e170b91c48f3d9312c0d7a86867049e49dbbe1b1f68c975"
-  version "2.4.11"
-  revision 27
+  url "https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_4_x/ViennaRNA-2.4.14.tar.gz"
+  #sha256 "ba9cfc8a48e457fc891628f3229a3924de31714460dc4a4dec081868f802cc28"
+  version "2.4.14"
+  revision 1
 
   depends_on "pkg-config"
   depends_on "mpfr"
   depends_on "wget"
   depends_on "gsl"
-
-  #bottle do
-  #  root_url "https://homebrew.bintray.com/bottles-core"
-  #  cellar :any
-  #  rebuild 1
-  #  sha256 "0e8ac9750f59ecde965e0c6f19735a7da4e755626f71139505f8906c5ec4a3cc" => :mojave
-  #end
-
+  depends_on "gcc@8"
   
   def install
-    system "wget", "https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_4_x/ViennaRNA-2.4.11.tar.gz"
-    system "tar", "xvzf", "ViennaRNA-2.4.11.tar.gz"
+    system "wget", "https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_4_x/ViennaRNA-2.4.14.tar.gz"
+    system "tar", "xvzf", "ViennaRNA-2.4.14.tar.gz"
     Dir.chdir("ViennaRNA-2.4.11")
     system "./configure", "-v",
       "--disable-debug",
@@ -31,8 +24,10 @@ class Viennarna < Formula
       "--without-perl",
       "--disable-openmp",
       "--prefix=#{prefix}",
-      "CC=/usr/bin/gcc", 
-      "CXX=/usr/bin/g++",
+      #"CC=/usr/bin/gcc", 
+      #"CXX=/usr/bin/g++",
+      "CC=gcc-8",
+      "CXX=g++-8",
       "CFLAGS=-march=skylake-avx512 -Wa,-march=skylake-avx512 -march=native" # -m64"
     system "make"
     ENV.deparallelize
