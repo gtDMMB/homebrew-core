39c481d53cd4811966cd25587c3c272488a5568a
Maxie D. Schmidt <maxieds@gmail.com>
2019-11-01 01:01:17 -0400

    Update viennarna.rb

--- Formula/viennarna.rb
+++ Formula/viennarna.rb
@@ -4,13 +4,13 @@ class Viennarna < Formula
   url "https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_4_x/ViennaRNA-2.4.14.tar.gz"
   #sha256 "ba9cfc8a48e457fc891628f3229a3924de31714460dc4a4dec081868f802cc28"
   version "2.4.14"
-  revision 2
+  revision 3
 
   depends_on "pkg-config"
   depends_on "mpfr"
   depends_on "wget"
   depends_on "gsl"
-  depends_on "gcc@8"
+  depends_on "llvm"
   
   def install
     system "wget", "https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_4_x/ViennaRNA-2.4.14.tar.gz"
@@ -26,8 +26,8 @@ class Viennarna < Formula
       "--prefix=#{prefix}",
       #"CC=/usr/bin/gcc", 
       #"CXX=/usr/bin/g++",
-      "CC=gcc-8",
-      "CXX=g++-8",
+      "CC=clang",
+      "CXX=clang++",
       "CFLAGS=-march=skylake-avx512 -Wa,-march=skylake-avx512 -march=native" # -m64"
     system "make"
     ENV.deparallelize
