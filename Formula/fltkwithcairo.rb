50058aa8063befbb000492644b49bec29ee06572
Maxie D. Schmidt <maxieds@gmail.com>
2019-11-01 06:24:40 -0400

    Update fltkwithcairo.rb

--- Formula/fltkwithcairo.rb
+++ Formula/fltkwithcairo.rb
@@ -4,14 +4,13 @@ class Fltkwithcairo < Formula
   url "https://www.fltk.org/pub/fltk/snapshots/fltk-1.4.x-20191025-4f8e692f.tar.gz"
   version "fltk-1.4.x-20191025-4f8e692f"
   sha256 "718a6469326c4b826c21434e3fe52b3aa911f57a93e8a935b0827c828a71e53b"
-  revision 3
+  revision 4
 
   depends_on "wget"
   depends_on "libffi"
   depends_on "cairo"
   depends_on "jpeg"
   depends_on "libpng"
-  depends_on "gcc@8"
   
   def install
     archcmd = "uname -m"
@@ -26,10 +25,10 @@ class Fltkwithcairo < Formula
       "--prefix=#{prefix}",
       "--enable-cairo",
       "--enable-threads",
-      #"CC=clang" + compiler_flags + " -arch " + sysarch + include_flags,
-      #"CXX=clang++" + compiler_flags + " -arch " + sysarch + include_flags,
-      "CC=gcc-8" + compiler_flags + " -arch " + sysarch + include_flags,
-      "CXX=g++-8" + compiler_flags + " -arch " + sysarch + include_flags,
+      "CC=clang" + compiler_flags + " -arch " + sysarch + include_flags,
+      "CXX=clang++" + compiler_flags + " -arch " + sysarch + include_flags,
+      #"CC=gcc-8" + compiler_flags + " -arch " + sysarch + include_flags,
+      #"CXX=g++-8" + compiler_flags + " -arch " + sysarch + include_flags,
       "CFLAGS=-march=skylake-avx512 -Wa,-march=skylake-avx512 -march=native" # -m64"
     ]
     system "make", "clean"
