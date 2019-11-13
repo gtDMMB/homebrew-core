c739697051b4eeede88d7354dfae40fddd76df4e
Maxie D. Schmidt <maxieds@gmail.com>
2019-11-13 09:45:43 -0500

    RNAStructViz -- v1.9.0-testing

--- Formula/rnastructviz.rb
+++ Formula/rnastructviz.rb
@@ -1,9 +1,9 @@
 class Rnastructviz < Formula
   desc "Graphical RNA secondary structure comparison multi-tool"
   homepage "https://github.com/gtDMMB/RNAStructViz/wiki"
-  url "https://github.com/gtDMMB/RNAStructViz/archive/v1.8.9-testing.tar.gz"
-  sha256 "990670ade51920c266ef81d9210116499da5a80ddfdb4c03a73d1d300b74277d"
-  version "v1.8.9-testing"
+  url "https://github.com/gtDMMB/RNAStructViz/archive/v1.9.0-testing.tar.gz"
+  sha256 "cd7c8ebd889cb841c2877ca812134eefd5042ce5ffc15b02615d2aa432f0425f"
+  version "v1.9.0-testing"
   revision 1
   
   depends_on "gtDMMB/core/fltkwithcairo"
@@ -17,7 +17,7 @@ class Rnastructviz < Formula
   def install
     system "make"
     bin.install "src/RNAStructViz"
-    prefix.install Dir["sample_structures"]
+    prefix.install Dir["sample-structures"]
   end
 
   test do
