c34bdcdea701feef34de7856b2c6b2026e1e9176
Maxie D. Schmidt <maxieds@gmail.com>
2019-11-20 20:56:31 -0500

    Update rnastructviz.rb

--- Formula/rnastructviz.rb
+++ Formula/rnastructviz.rb
@@ -4,13 +4,14 @@ class Rnastructviz < Formula
   url "https://github.com/gtDMMB/RNAStructViz/archive/v1.9.6-testing.tar.gz"
   sha256 "795e4e2b3558b7d993d4255f05c8d7943c4f8ba4bcc3654cb47335b18f62bb6e"
   version "v1.9.6-testing"
-  revision 1
+  revision 2
   
   depends_on "gtDMMB/core/fltkwithcairo"
   depends_on "gtDMMB/core/viennarna"
   depends_on "pkg-config"
   depends_on "curl"
   depends_on "coreutils"
+  depends_on "grep"
   depends_on "openssl"
   depends_on "boost"
 
