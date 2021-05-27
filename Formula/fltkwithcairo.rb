a95c031d100513f69e44510502c7edb1553f2e13
Maxie D. Schmidt <maxieds@gmail.com>
2021-05-27 17:59:51 -0400

    Update fltkwithcairo.rb

--- Formula/fltkwithcairo.rb
+++ Formula/fltkwithcairo.rb
@@ -4,7 +4,7 @@ class Fltkwithcairo < Formula
   url "https://github.com/gtDMMB/homebrew-core/raw/master/LocalPatches/fltk-1.4.x-20210514-cbee4880.tar.gz"
   version "fltk-1.4.x-20210514-cbee4880"
   #sha256 "158798350391552af635c73b99e085367d1a8f58525503bfab94134583e8e15c"
-  revision 5
+  revision 6
 
   depends_on "wget"
   depends_on "libffi"
@@ -17,6 +17,12 @@ class Fltkwithcairo < Formula
     sysarch = `#{archcmd}`.tr("\n", "")
     compiler_flags = " -g -DBUILD_SHARED_LIBS -D__APPLE__ -D__APPLE_QUARTZ__"
     swversion = `#{"sw_vers"}`.tr("\n", "");
+    cflags_x86_64 = "-march=skylake-avx512 -Wa,-march=skylake-avx512 -march=native -m64"
+    cflags_other = "-mprefer-vector-width=256 -march=native -Wa,-march=native -m64"
+    cflags = cflags_other
+    if archcmd == "x86_64"
+      cflags = cflags_x86_64
+    end
     if swversion[3..4].ord >= 14
       compiler_flags += " -DROTATE";
     end
@@ -28,7 +34,8 @@ class Fltkwithcairo < Formula
       "--enable-threads",
       "CC=clang" + compiler_flags + " -arch " + sysarch + include_flags,
       "CXX=clang++" + compiler_flags + " -arch " + sysarch + include_flags,
-      "CFLAGS=-mprefer-vector-width=256 -march=native -Wa,-march=native -m64"
+      "CFLAGS=" + cflags
+      #"CFLAGS=-mprefer-vector-width=256 -march=native -Wa,-march=native -m64"
       #"CFLAGS=-march=skylake-avx512 -Wa,-march=skylake-avx512 -march=native -m64"
     ]
     system "make", "clean"
