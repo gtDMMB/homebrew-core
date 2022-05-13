cc5f3c3fb5eeba45661029110c308d1384678c9c
Maxie D. Schmidt <maxieds@gmail.com>
2022-05-13 01:38:23 -0400

    Update and rename autoconf.rb to autoconf2.rb

--- Formula/autoconf2.rb
+++ Formula/autoconf2.rb
@@ -1,10 +1,10 @@
-class Autoconf < Formula
+class Autoconf2 < Formula
   
   desc "A custom version of autoconf known to work with the build scripts for gtDMMB/core/gtfoldpython"
   url "ftp://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz"
   version "2.69"
   sha256 "954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969"
-  revision 1
+  revision 2
   
   def install
     system "./configure"
