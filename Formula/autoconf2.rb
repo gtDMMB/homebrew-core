44e3318305dc65b6e073e7ef92bea5aa08d9de77
Maxie D. Schmidt <maxieds@gmail.com>
2022-05-13 01:45:49 -0400

    Update autoconf2.rb

--- Formula/autoconf2.rb
+++ Formula/autoconf2.rb
@@ -4,7 +4,9 @@ class Autoconf2 < Formula
   url "ftp://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz"
   version "2.69"
   sha256 "954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969"
-  revision 2
+  revision 3
+  
+  depends_on :macos
   
   def install
     system "./configure"
