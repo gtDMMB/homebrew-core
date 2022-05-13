260d049207d1a25d0914c9b4ea9628f15a4eb059
Maxie D. Schmidt <maxieds@gmail.com>
2022-05-13 01:27:59 -0400

    Create autoconf.rb

---
+++ Formula/autoconf.rb
@@ -0,0 +1,25 @@
+class Autoconf < Formula
+  
+  desc "A custom version of autoconf known to work with the build scripts for gtDMMB/core/gtfoldpython"
+  url "ftp://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz"
+  version "2.69"
+  sha256 "954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969"
+  revision 1
+  
+  def install
+    system "./configure"
+    system "make"
+    bin.install "bin/autoconf"
+    bin.install "bin/autoheader"
+    bin.install "bin/autom4te"
+    bin.install "bin/autoreconf"
+    bin.install "bin/autoscan"
+    bin.install "bin/autoupdate"
+    bin.install "bin/ifnames"
+  end
+  
+  test do
+    system "false"
+  end
+  
+end
