cb2671aa603e8ddfe6f6bb4f65e398dc3948d1cf
Maxie D. Schmidt <maxieds@gmail.com>
2022-05-13 03:08:49 -0400

    Update autoconf2.rb

--- Formula/autoconf2.rb
+++ Formula/autoconf2.rb
@@ -4,7 +4,7 @@ class Autoconf2 < Formula
   url "ftp://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz"
   version "2.69"
   sha256 "954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969"
-  revision 4
+  revision 5
   
   depends_on :macos
   
@@ -16,10 +16,11 @@ class Autoconf2 < Formula
     end
     system "./configure"
     system "make"
-    bin.install "bin/autoconf"
+    system "make", "install"
+    #bin.install "bin/autoconf"
     #bin.install "bin/autoheader"
     #bin.install "bin/autom4te"
-    bin.install "bin/autoreconf"
+    #bin.install "bin/autoreconf"
     #bin.install "bin/autoscan"
     #bin.install "bin/autoupdate"
     #bin.install "bin/ifnames"
