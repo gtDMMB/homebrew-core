6a5400705767ab12f9feedfca845a96807af4226
Maxie D. Schmidt <maxieds@gmail.com>
2022-05-13 03:09:27 -0400

    Update automake2.rb

--- Formula/automake2.rb
+++ Formula/automake2.rb
@@ -4,7 +4,7 @@ class Automake2 < Formula
   url "ftp://ftp.gnu.org/gnu/automake/automake-1.16.2.tar.gz"
   version "1.16.2"
   sha256 "b2f361094b410b4acbf4efba7337bdb786335ca09eb2518635a09fb7319ca5c1"
-  revision 6
+  revision 7
   
   depends_on "gtDMMB/core/autoconf2"
   depends_on :macos
@@ -12,13 +12,14 @@ class Automake2 < Formula
   def install
     system "./configure"
     system "make"
-    bin.install "bin/automake"
-    bin.install "bin/aclocal"
-    bin.install "bin/aclocal-1.16"
-    bin.install "bin/automake-1.16"
-    system "mkdir", "-p", "Automake"
-    system "cp", "-p", "lib/Automake/Config.pm", "Automake/Config.pm"
-    bin.install "Automake/Config.pm"
+    system "make", "install"
+    #bin.install "bin/automake"
+    #bin.install "bin/aclocal"
+    #bin.install "bin/aclocal-1.16"
+    #bin.install "bin/automake-1.16"
+    #system "mkdir", "-p", "Automake"
+    #system "cp", "-p", "lib/Automake/Config.pm", "Automake/Config.pm"
+    #bin.install "Automake/Config.pm"
   end
   
   test do
